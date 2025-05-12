ba7b72b643b5bd3850fd3b91a64fc47ac4d6e65b
McDjXdLol <michal.jereczek2004@gmail.com>
2025-05-12 13:45:00 +0200

    fix: repaired run_game.py
    So run_game.py wasn't running as it should but now the problem is solved. It should work without problems.

--- run_game.py
+++ run_game.py
@@ -1,4 +1,14 @@
+import sys
 import os
 
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'hero_of_embers'))
+if project_path not in sys.path:
+    sys.path.append(project_path)
+
+from hero_of_embers.main import main
+
 if __name__ == "__main__":
-    os.system("python hero_of_embers/main.py")
+    game = main()
+    game.main()
+    
+    
